c00c47ca00febcfb510fe6f49e814307d029cb27
Prem Sawant <premsawant1802@gmail.com>
2026-02-28 13:39:15 +0530

    Complete medtech project

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import { signIn } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import { useState } from "react";
 import Image from "next/image";
 import { Lexend } from "next/font/google";
-
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 import { FaFacebook, FaTwitter, FaGoogle } from "react-icons/fa";
 
 
@@ -13,13 +14,24 @@ const lexend = Lexend({
 });
 
 export default function LoginPage() {
+  const { data: session, status } = useSession();
+  const router = useRouter();
   const [email, setEmail] = useState("");
-const [password, setPassword] = useState("");
+  const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    if (status === "authenticated") {
+      if (session?.user?.role === "admin") {
+        router.push("/admin");
+      } else {
+        router.push("/");
+      }
+    }
+  }, [status, session, router]);
   return (
     <main
       className={`relative min-h-screen flex items-center justify-center ${lexend.className}`}
     >
-      {/* Background Image */}
       <div className="absolute inset-0 -z-10">
         <Image
           src="/images/amlaBg.png"
@@ -27,69 +39,60 @@ const [password, setPassword] = useState("");
           fill
           className="object-cover "
         />
-        {/* Soft Overlay */}
         <div className="absolute bg-white/40 inset-0 backdrop-blur-sm"></div>
       </div>
 
-      {/* Login Card */}
       <div className="w-120 bg-[#F5F3EE] rounded-[40px] shadow-xl px-10 py-14">
         <h1 className="text-4xl font-bold text-center mb-10 text-black">
           Login
         </h1>
 
-        {/* Username */}
         <div className="mb-8">
           <label className="block mb-2 text-lg font-medium text-black">
             Username
           </label>
-         <input
-  type="text"
-  placeholder="Type your email"
-  value={email}
-  onChange={(e) => setEmail(e.target.value)}
-  className="..."
-/>
+          <input
+            type="text"
+            placeholder="Type your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="..."
+          />
         </div>
-
-        {/* Password */}
         <div className="mb-4">
           <label className="block mb-2 text-lg font-medium text-black">
             Password
           </label>
           <input
-  type="password"
-  placeholder="Type your password"
-  value={password}
-  onChange={(e) => setPassword(e.target.value)}
-  className="..."
-/>
+            type="password"
+            placeholder="Type your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="..."
+          />
         </div>
 
         <div className="text-right text-sm text-gray-600 mb-8 cursor-pointer hover:underline">
           Forgot password?
         </div>
 
-        {/* Login Button */}
         <button
-  onClick={async () => {
-    const result = await signIn("credentials", {
-      email,
-      password,
-      redirect: false,
-    });
+          onClick={async () => {
+            const result = await signIn("credentials", {
+              email,
+              password,
+              redirect: false,
+            });
 
-    if (result?.error) {
-      alert("Invalid credentials");
-    } else {
-      window.location.href = "/";
-    }
-  }}
-  className="w-full bg-[#9A6B4C] text-white py-4 rounded-full text-xl font-semibold"
->
-  Login
-</button>
+            if (result?.error) {
+              alert("Invalid credentials");
+            }
+          }}
+          className="w-full bg-[#9A6B4C] text-white py-4 rounded-full text-xl font-semibold"
+        >
+          Login
+        </button>
 
-        {/* Social Login */}
         <div className="text-center mt-6 text-gray-700">
           or Sign Up using
         </div>
@@ -102,7 +105,14 @@ const [password, setPassword] = useState("");
             <FaTwitter className="h-10 w-10 cursor-pointer hover:scale-110 transition text-blue-400" />
           </div>
           <div>
-            <FaGoogle className="h-10 w-10 cursor-pointer hover:scale-110 transition text-red-500" />
+            <FaGoogle
+              onClick={() =>
+                signIn("google", {
+                  callbackUrl: "/",
+                })
+              }
+              className="h-10 w-10 cursor-pointer hover:scale-110 transition text-red-500"
+            />
           </div>
         </div>
 
